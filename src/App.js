9853ac55739093e33d9618b34a792d6ed2d77cf0
Siwyus <siwyus@gmail.com>
2019-10-11 19:07:40 +0200

    Clean up project and create template in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1 className="text-center">My Items App</h1>
+      <p className="text-center">
+        Save your items for not to forget them while moving!
+      </p>
+      <Row>
+        <Col>
+          <h2> Add Item </h2>
+        </Col>
+        <Col>
+          <h2> Items </h2>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
